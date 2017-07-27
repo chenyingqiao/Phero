8022dde9c5ff62caabb969609771aebb1f49aff3
lerko <yingqiao_chen@ziguangcn.com>
2017-07-27 18:02:09 +0800

    添加说明文档章节

---
+++ src/Phero/Map/Note/Resource.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * @Author: lerko
+ * @Date:   2017-07-27 17:59:15
+ * @Last Modified by:   lerko
+ * @Last Modified time: 2017-07-27 18:01:13
+ */
+
+class Resource
+{
+	public $name;//资源的名称 可以是某台服务器
+	public $rule;//资源的规则  按照那些条件区寻找相应的资源
+}
